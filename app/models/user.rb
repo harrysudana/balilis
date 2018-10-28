d474e34a6e6f401e7bd62e0a14148134f37bdd4c
harrysudana <harrysudana@gmail.com>
2018-10-28 11:45:53 +0800

    add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  after_initialize :set_default_role
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
+
+  enum role:[:user,:owner,:admin]
+
+  def set_default_role
+  	self.role ||= :user
+  end
 end
