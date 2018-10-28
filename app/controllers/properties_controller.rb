5254777834920d1f490c3fe7f65d13d6642ec9a0
harrysudana <harrysudana@gmail.com>
2018-10-28 11:57:33 +0800

    property controller and model relation

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,42 @@
 class PropertiesController < ApplicationController
+	before_action :set_property, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, :except => [:index]
+
+	def create
+		@property = Property.new(property_params)
+		authorize @property
+
+		if @property.save
+			redirect_to @property, alert: "Property offer succesfully created"
+		else
+			render new_property_path, alert: "Oops!, please try again!"
+		end
+	end
+
+	def edit
+		authorize @property
+	end
+
+	def update
+		authorize @property
+		@property.update(property_params)
+		redirect_to @property, alert: "Property offer succesfully updated"
+	end
+
+	def destroy
+		authorize @property
+		@property.destroy
+		redirect_to root_path, alert: "Property offer succesfully deleted"
+	end
+
+	private
+		def set_property
+			@property = Property.find(params[:id])
+		end
+
+		def property_params
+			params.require(:property).permit(:title, :description, :url, :location, :category_id, :owner_id)
+		end
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   after_initialize :set_default_role
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   devise :omniauthable, :omniauth_providers => [:facebook]
 
   enum role:[:user,:owner,:admin]
+  has_many :properties, :foreign_key => 'owner_id' 
 
   def set_default_role
   	self.role ||= :user
