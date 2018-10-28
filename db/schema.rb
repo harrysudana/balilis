651fb9d5115287d8feaf5d2e6c15ba80f948d9e7
harrysudana <harrysudana@gmail.com>
2018-10-28 11:40:21 +0800

    migrate Property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_032053) do
+ActiveRecord::Schema.define(version: 2018_10_28_032959) do
+
+  create_table "properties", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.string "location"
+    t.integer "owner_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
